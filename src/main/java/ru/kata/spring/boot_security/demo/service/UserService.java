c610c345af3c5375ac77a4fc4bf9eae5d6dffb82
amaribel <cereviziae@mail.ru>
2023-01-17 05:30:53 +0300

    ready

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,83 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.security.InvalidParameterException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, @Lazy BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        return user;
+    }
+
+
+    @Transactional
+    public void updateUser(User user, Set<Role> roles, int id) {
+        User userFromDb = getUserById(user.getId());
+        if (!userFromDb.getPassword().equals(user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+        user.setId(id);
+        saveUser(user, roles);
+    }
+
+    public List<Role> listRoles() {
+        return roleRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void saveUser(User user, Set<Role> roles) {
+        user.setRoles(roles);
+        roles.forEach(s -> s.setUsers(new HashSet<>(Collections.singleton(user))));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        roleRepository.saveAll(roles);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUserById(int id) {
+        userRepository.delete(getUserById(id));
+    }
+
+}
